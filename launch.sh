4065fd244ae98309045c30f6f886602a40fd4733
Jonathan Bowman <git@bowmanjd.com>
2026-04-12 14:04:54 -0400

    fix launch script

--- launch.sh
+++ launch.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 podman run -d \
+	--replace \
   --name llama-cuda \
   -p 8000:8000 \
-  -v ~/.cache/huggingface:/cache \
-  -e HF_CACHE_PATH=/cache \
+	-v ~/.cache/huggingface/hub:/hub \
+  -e HF_HUB_CACHE=/hub \
   ghcr.io/bowmanjd/llama-cpp-cuda:b8762-cuda13
