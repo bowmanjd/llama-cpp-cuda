3382dfb8e05a0c6eeb432d79fa2a856cd195e0cd
Jonathan Bowman <jbowman@cargas.com>
2026-04-12 16:53:27 -0400

    better launch

--- launch.sh
+++ launch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+HUB_PATH="${1:-${HF_HUB_CACHE:-${HOME}/.cache/huggingface/hub}}"
+
 podman run -d \
 	--replace \
   --name llama-cuda \
   -p 8000:8000 \
-	-v ~/.cache/huggingface/hub:/hub \
+	-v "${HUB_PATH}:/hub" \
   -e HF_HUB_CACHE=/hub \
   ghcr.io/bowmanjd/llama-cpp-cuda:b8762-cuda13
